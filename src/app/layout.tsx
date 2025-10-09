4e2f07cb257bedfb88ff464af7e89ed874bdd38f
Cottier <cottis2@medtronic.com>
2025-10-09 13:37:28 +0200

    ðŸŽ¨ Update theme colors for favicon preparation
    - Changed theme-color meta tag to #071531
    - Updated msapplication-TileColor to #071531
    - Updated Safari mask-icon color to #071531
    - Prepared site for whale logo favicon integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,15 +46,15 @@ export default function RootLayout({
         <link
           rel="mask-icon"
           href="/favicon/safari-pinned-tab.svg"
-          color="#000000"
+          color="#071531"
         />
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="msapplication-TileColor" content="#071531" />
         <meta
           name="msapplication-config"
           content="/favicon/browserconfig.xml"
         />
-        <meta name="theme-color" content="#000" />
+        <meta name="theme-color" content="#071531" />
         <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       </head>
       <body className={cn(inter.className)} style={{backgroundColor: '#071531'}}>
